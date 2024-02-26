3315407aa10e85eb106dfc27e68ce6cee2d512b3
alexiyous <alexiusrenanta@gmail.com>
2024-02-26 11:27:00 +0700

    feat: Add Popup Wiring Instruction
    Create a clip board for a popup instruction trigger. When grabbed, it will trigger a popup canvas telling an installation instruction

---
+++ Assets/Scripts/Alex/WiringPopUp.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Seville;
+using TMPro;
+
+public class WiringPopUp : PopUpController
+{
+    [SerializeField] private TextMeshProUGUI headerTMP;
+    [SerializeField] private string headerText;
+
+    public void OpenPopup()
+    {
+        headerTMP.text = headerText;
+        OnClickOpenPopup();
+    }
+}
