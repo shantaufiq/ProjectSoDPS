92c7e0260e5810db704e8c5fa4123eac14bff37b
RaflyGG <123481629+RaflyGG@users.noreply.github.com>
2024-03-06 17:11:03 +0700

    feat: New quest mechanic and UI
    menambahkan mekanik quest baru dan UI quest baru

---
+++ Assets/Scripts/Perencanaan Dan Persiapan IoT/ToDoController.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace PerencanaanPersiapanIoT
+{
+    public class ToDoController : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct ItemList
+        {
+            public string itemName;
+            public bool isCompleted; // Menyimpan status quest
+        }
+        
+        public List<ItemList> itemLists;
+        public ToDoHandler prefabQuest;
+        public Transform itemListParent;
+
+        void Start()
+        {
+            DisplayItemList();
+        }
+
+        void DisplayItemList()
+        {
+            EraseCanvas();
+            foreach (var item in itemLists)
+            {
+                ToDoHandler questItem = Instantiate(prefabQuest, itemListParent);
+                questItem.SetToDoHandler(item.isCompleted, item.itemName);
+            }
+        }
+
+        public void EraseCanvas()
+        {
+            for (int child = 0; child < itemListParent.childCount; child++)
+            {
+                Destroy(itemListParent.transform.GetChild(child).gameObject);
+            }
+        }
+
+        public void FinishItem(int index)
+        {
+            var questData = itemLists;
+
+            if (index > questData.Count - 1)
+            {
+                return;
+            }
+
+            if (!questData[index].isCompleted)
+            {
+                var temp = questData[index];
+                temp.isCompleted = true;
+
+                questData[index] = temp;
+            }
+
+            DisplayItemList();
+        }
+    }
+}
