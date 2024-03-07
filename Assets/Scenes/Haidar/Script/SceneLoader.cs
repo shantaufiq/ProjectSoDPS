761447a48e8cdfd4915c7e7f962e772926e80c23
Haidar341 <haizak3@gmail.com>
2024-03-07 13:32:45 +0700

    feat: Menyelsaikan semua stage dan menmbahkan asset
    menyelsaikan logic dan memasukan aset dan semua ui kecuali ui level selection

---
+++ Assets/Scenes/Haidar/Script/SceneLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string nextSceneName; // Nama scene berikutnya yang akan dimuat
+    public float delayBeforeLoading = 5f; // Waktu tunda sebelum memuat scene berikutnya
+
+    void Start()
+    {
+        // Memulai timer untuk memuat scene berikutnya setelah delay tertentu
+        Invoke("LoadNextScene", delayBeforeLoading);
+    }
+
+    void LoadNextScene()
+    {
+        // Memuat scene berikutnya jika namanya tidak kosong
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.LogError("Next scene name is not set!");
+        }
+    }
+}
+
