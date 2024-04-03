53c143548d95e67ed6efb4f729c150f0f23296d8
Haidar341 <haizak3@gmail.com>
2024-04-03 15:44:49 +0700

    feat : add audio to scene

--- Assets/Scenes/Haidar/Script/SceneLoader.cs
+++ Assets/Scenes/Haidar/Script/SceneLoader.cs
@@ -8,25 +8,14 @@ public class SceneLoader : MonoBehaviour
     public string nextSceneName; // Nama scene berikutnya yang akan dimuat
     public float delayBeforeLoading = 5f; // Waktu tunda sebelum memuat scene berikutnya
 
-    private AudioSource[] audioSources; // Array untuk menyimpan semua komponen AudioSource di scene
-
     void Start()
     {
-        // Memperoleh semua komponen AudioSource di scene
-        audioSources = FindObjectsOfType<AudioSource>();
-
         // Memulai timer untuk memuat scene berikutnya setelah delay tertentu
         Invoke("LoadNextScene", delayBeforeLoading);
     }
 
     void LoadNextScene()
     {
-        // Mematikan semua audio di scene sebelum memuat scene berikutnya
-        foreach (var audioSource in audioSources)
-        {
-            audioSource.Stop();
-        }
-
         // Memuat scene berikutnya jika namanya tidak kosong
         if (!string.IsNullOrEmpty(nextSceneName))
         {
